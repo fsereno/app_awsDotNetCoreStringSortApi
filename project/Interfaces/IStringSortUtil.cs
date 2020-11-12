bc23fc39a7105ff6108bb97fe70c12c08278ba8b
Fabio Sereno <fabio_sereno@hotmail.com>
2020-11-12 16:11:20 +0000

    Resources now in repo and published from this location

---
+++ project/Interfaces/IStringSortUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+
+namespace Interfaces
+{
+   public interface IStringSortUtil
+   {
+       string Sort(string commaSeperatedString);
+
+       List<SortItem> GetSortItems(string commaSeperatedString);
+
+       string Join(List<SortItem> sortedCharacters);
+   }
+}
