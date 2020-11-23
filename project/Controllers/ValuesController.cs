c88d8545f267bb3ee72d594febf20890f89fe60b
Fabio Sereno <fabio_sereno@hotmail.com>
2020-11-23 11:08:36 +0000

    Tidy up after deployment of heads now in startup

--- project/Controllers/ValuesController.cs
+++ project/Controllers/ValuesController.cs
@@ -15,15 +15,8 @@ namespace aws.Controllers
         [HttpPost("sort")]
         public SortResult Sort([FromBody] SortRequest request)
         {
-            //SetResponseHeaders();
             var result = _stringSortUtil.Sort(request.CommaSeperatedString);
             return new SortResult(){ Result = result };
         }
-
-        /*private void SetResponseHeaders() {
-            Response.Headers.Add("Access-Control-Allow-Methods", "POST");
-            Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token");
-            Response.Headers.Add("Access-Control-Allow-Origin", "*");
-        }*/
     }
 }
